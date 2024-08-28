d4efe509e63690c5859c6cd38422ba2b99544163
ShrushtiK <31542248+ShrushtiK@users.noreply.github.com>
2024-08-28 09:54:33 +0200

    Update AlexNet entirely

--- training/models/alexnet.py
+++ training/models/alexnet.py
@@ -22,8 +22,10 @@ class AlexNet(nn.Module):
         )
         self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
         self.classifier = nn.Sequential(
+            nn.Dropout(0.5),
             nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
             nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
             nn.Linear(4096, num_classes)
